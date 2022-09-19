019b07c11217c1af01b220b7266b5db05083d0b3
Adam Purcell <adam88183@gmail.com>
2022-09-18 23:13:09 -0400

    More accurate tests

--- tests.py
+++ tests.py
@@ -10,14 +10,14 @@ switchedWins = 0
 switchedLosses = 0
 
 # Specify number of iterations
-iterations = 500
+iterations = 10000000
 
 for i in range(iterations):
     progress = ((i + 1) / iterations) * 100
     if progress % 1 == 0:
         clear()
         print("Progress: " + str(round(progress, 2)) + "%")
-    result = doors.game(random.randint(1, 3), random.randint(0, 1))
+    result = doors.game(random.randint(1, 3), 1) # Switch is always true
     if result[0]:
         if result[1]:
             switchedWins += 1
