a1663377ceb0c2b034ec56280dcfcd93a823cba8
Adam Purcell <adam88183@gmail.com>
2022-09-18 22:12:38 -0400

    All completed.

---
+++ doors.py
@@ -0,0 +1,81 @@
+import random
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def game(): # For user input
+    clear()
+    doors = [1, 2, 3]
+    prizeIndex = random.randint(0, 2)
+    firstChoice = None
+
+    while firstChoice not in doors:
+        try:
+            firstChoice = int(input("Pick a door: 1, 2, or 3\nChoice: "))
+            if firstChoice not in doors:
+                print("Please pick a valid door.")
+            else:
+                continue
+        except ValueError:
+            print("Invalid input. Try again.")
+
+    openedDoor = None
+
+    for i in doors:
+        if i is not firstChoice and i is not doors[prizeIndex]:
+            openedDoor = i
+            doors.remove(i)
+            break    
+
+    doors.remove(firstChoice)
+    remainingDoor = doors[0]
+
+    choseSwitch = input("Door " + str(openedDoor) + " was opened.\nSwitch doors? (Y/N)\nChoice: ")
+
+    switched = (choseSwitch == "Y" or choseSwitch == "y")
+    won = False
+
+    if switched:
+        switched = True
+        print("You chose to switch to door " + str(remainingDoor) + ".")
+        if remainingDoor == prizeIndex + 1:
+            won = True
+            print("You got the prize!")
+        else:
+            print("You lost.")
+    else:
+        if firstChoice == prizeIndex + 1:
+            won = True
+            print("You got the prize!")
+        else:
+            print("You lost.")
+
+    return [switched, won]
+
+def game(choice, switch): # For automated testing
+    doors = [1, 2, 3]
+    prizeIndex = random.randint(0, 2)
+
+    firstChoice = choice
+
+    for i in doors:
+        if i is not firstChoice and i is not doors[prizeIndex]:
+            doors.remove(i)
+            break    
+
+    doors.remove(firstChoice)
+    remainingDoor = doors[0]
+
+    switched = switch
+    won = False
+
+    if switched:
+        switched = True
+        if remainingDoor == prizeIndex + 1:
+            won = True
+    else:
+        if firstChoice == prizeIndex + 1:
+            won = True
+
+    return [switched, won]
---
+++ tests.py
@@ -0,0 +1,29 @@
+import doors
+import random
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear()
+switchedWins = 0
+switchedLosses = 0
+
+# Specify number of iterations
+iterations = 500
+
+for i in range(iterations):
+    progress = ((i + 1) / iterations) * 100
+    if progress % 1 == 0:
+        clear()
+        print("Progress: " + str(round(progress, 2)) + "%")
+    result = doors.game(random.randint(1, 3), random.randint(0, 1))
+    if result[0]:
+        if result[1]:
+            switchedWins += 1
+        else:
+            switchedLosses += 1
+
+print("\nResults:\n")
+print("Switched wins: " + str(switchedWins) + "\nSwitched losses: " + str(switchedLosses) + "\n")
+print("Win rate when switiching: " + str((switchedWins / (switchedWins + switchedLosses)) * 100) + "%\n")
