b2b861b30b1799d604c66c078d64004f618cc7de
adarax <adam88183@gmail.com>
2023-04-07 15:42:48 -0400

    Working product

--- tests.py
+++ tests.py
@@ -2,8 +2,10 @@ import doors
 import random
 import os
 
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 clear()
 switchedWins = 0
@@ -14,16 +16,21 @@ iterations = 100000
 
 for i in range(iterations):
     progress = ((i + 1) / iterations) * 100
+    
     if progress % 1 == 0:
         clear()
         print("Progress: " + str(round(progress, 2)) + "%")
-    result = doors.game(random.randint(1, 3), 1) # Switch is always true
-    if result[0]:
-        if result[1]:
-            switchedWins += 1
-        else:
-            switchedLosses += 1
+    
+    switchDoors = False
+    result = doors.game(random.randint(1, 3), switchDoors)  # Parameters are (door # chosen, switch)
+    
+    if result[1]:
+        switchedWins += 1
+    else:
+        switchedLosses += 1
 
 print("\nResults:\n")
-print("Switched wins: " + str(switchedWins) + "\nSwitched losses: " + str(switchedLosses) + "\n")
-print("Win rate when switiching: " + str((switchedWins / (switchedWins + switchedLosses)) * 100) + "%\n")
+print("Switched wins: " + str(switchedWins) +
+      "\nSwitched losses: " + str(switchedLosses) + "\n")
+print("Win rate when " + ("" if result[0] else "not ") + "switiching doors: " +
+      str((switchedWins / (switchedWins + switchedLosses)) * 100) + "%\n")
